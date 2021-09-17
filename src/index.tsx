907118369b683e073cb7d8354f0ef750e81fdd3f
Silvia Zeni <silvia.szn@gmail.com>
2021-09-17 17:15:10 +0100

    feat: Add files

---
+++ src/index.tsx
@@ -0,0 +1,72 @@
+import GoogleMapReact from "google-map-react";
+import { PositionMeta, PositionData, HeatmapData } from "./interfaces";
+
+export default class GeolocationMap {
+  name: string;
+
+  icon: string;
+
+  tooltip: string;
+
+  usesModal: boolean;
+
+  constructor() {
+    this.name = "Geolocation Map";
+    this.icon = "places";
+    this.tooltip = "Plot geolocation heatmap";
+    this.usesModal = true;
+  }
+
+  getData = (metadata: PositionMeta): PositionData =>
+    metadata
+      .filter((mitem) => mitem?.gps?.latitude && mitem?.gps?.longitude)
+      .map(({ gps }) => ({
+        lat: gps.latitude,
+        lng: gps.longitude,
+        weight: 1,
+      }));
+
+  getCenter = (positionData: PositionData): number[] => {
+    function getSum(data: PositionData, key: string): number {
+      return data.map((d) => d[key] as number).reduce((a, b) => a + b);
+    }
+
+    return [
+      getSum(positionData, "lat") / positionData.length,
+      getSum(positionData, "lng") / positionData.length,
+    ];
+  };
+
+  onClick = (metadata: PositionMeta): JSX.Element | null => {
+    if (!metadata) return null;
+
+    const defaultZoom = 1;
+
+    const positionData: PositionData = this.getData(metadata);
+
+    const heatmapCenter = this.getCenter(positionData);
+
+    const heatmapData: HeatmapData = {
+      positions: positionData,
+      options: {
+        radius: 20,
+        opacity: 1,
+      },
+    };
+
+    return positionData.length > 0 ? (
+      <>
+        <GoogleMapReact
+          defaultZoom={defaultZoom}
+          defaultCenter={heatmapCenter}
+          heatmap={heatmapData}
+          bootstrapURLKeys={{
+            // TODO: comment in line below when an api key is set
+            // key: import.meta.env.VITE_GMAPS_API_KEY,
+            libraries: ["places", "visualization"],
+          }}
+        />
+      </>
+    ) : null;
+  };
+}
